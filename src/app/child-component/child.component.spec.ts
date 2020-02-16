456c4229733e22ba3ce8d65cbcb65b264dccc2ff
Pierre-Alain EMO <pierre.alain.emo@gmail.com>
2020-02-16 15:30:24 +0100

    Mock function before injection is easier to modify for later usage

--- src/app/child-component/child.component.spec.ts
+++ src/app/child-component/child.component.spec.ts
@@ -3,8 +3,8 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ChildComponent} from './child.component';
 import {DependencyChildService} from '../dependency-child.service';
 
-const DependencyChildServiceMock = {
-  doSomething: () => 'Return from mock'
+const dependencyChildServiceMock = {
+  doSomething: jest.fn().mockReturnValue('Return from mock')
 };
 
 describe('ChildComponentComponent', () => {
@@ -12,40 +12,33 @@ describe('ChildComponentComponent', () => {
   let fixture: ComponentFixture<ChildComponent>;
 
   beforeEach(async(() => {
-    component = null;
-    fixture = null;
     TestBed.configureTestingModule({
       declarations: [ChildComponent],
-      providers: [{provide: DependencyChildService, useValue: DependencyChildServiceMock}]
-    });
-    // .compileComponents();
-    // For this case, we do not compile components here because we want to override provider in a specific case
-  }));
+      providers: [{provide: DependencyChildService, useValue: dependencyChildServiceMock}]
+    })
+      .compileComponents();
 
-  it('should create', () => {
     fixture = TestBed.createComponent(ChildComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  }));
+
+  it('should create', () => {
     expect(component).toBeTruthy();
+    expect(dependencyChildServiceMock.doSomething).toHaveBeenCalled();
   });
 
   it('should render without any input', () => {
-    fixture = TestBed.createComponent(ChildComponent);
-    fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
 
   it('should render with inputs filled and specific mock', () => {
-    TestBed
-      .overrideProvider(
-        DependencyChildService,
-        {useValue: {doSomething: () => 'Another specific return'}})
-      .compileComponents();
-    fixture = TestBed.createComponent(ChildComponent);
-    component = fixture.componentInstance;
+    dependencyChildServiceMock.doSomething.mockReturnValue('Another specific return');
+
     component.name = 'John';
     component.age = 42;
     fixture.detectChanges(); // detectChanges() is needed here because Inputs are added
+
     expect(fixture).toMatchSnapshot();
   });
 });
